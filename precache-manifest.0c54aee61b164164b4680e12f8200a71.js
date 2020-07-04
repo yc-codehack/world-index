2936352f40157099c10b0095100016c3a46bb53d
yc <learnnewthighs@gmail.com>
2020-07-04 20:26:57 +0530

    Updates

---
+++ precache-manifest.0c54aee61b164164b4680e12f8200a71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e15ba74709bd47aa3109d95c6745c94a",
+    "url": "/world-index/index.html"
+  },
+  {
+    "revision": "5c2f8af2d1ee9f655c4e",
+    "url": "/world-index/static/css/main.37004085.chunk.css"
+  },
+  {
+    "revision": "79f8776e025bba8fb8dd",
+    "url": "/world-index/static/js/2.6c8f77f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/world-index/static/js/2.6c8f77f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c2f8af2d1ee9f655c4e",
+    "url": "/world-index/static/js/main.0b75638e.chunk.js"
+  },
+  {
+    "revision": "4567b694e9664d3872e9",
+    "url": "/world-index/static/js/runtime-main.61408bd9.js"
+  }
+]);
